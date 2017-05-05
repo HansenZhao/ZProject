4fd4c25db5a99e6e8271d26bd638b2b0f00fdcc1
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-05-05 15:50:51 +0800

    add a few functions to get physical features at certain frame

--- ParticleData.m
+++ ParticleData.m
@@ -123,49 +123,111 @@ classdef ParticleData < handle
             particleNum = sum(obj.particleData(:,2) == frameIndex);
         end
         
-        function parId = getParticleIDAtTime(obj,frameIndex)
-            filter = obj.particleData(:,2) == frameIndex;
+        function parId = getParticleIDAtTime(obj,frame)
+            filter = obj.particleData(:,2) == frame ;
             parId = obj.particleData(filter,1);
         end
         
-        function [ids,pos,dir,tDir] = getParticleDirAtTime(obj,frameIndex,isRef,backLength)
-            ids = intersect(obj.getParticleIDAtTime(frameIndex-backLength),...
-                            obj.getParticleIDAtTime(frameIndex));
+        function [ids,pos,data] = velocityAtFrame(obj,frame,backLength,isRef,isDir)
+            refFrame = frame - backLength;
+            ids = intersect(obj.getParticleIDAtTime(refFrame),...
+                            obj.getParticleIDAtTime(frame));
             L = length(ids);
             pos = zeros(L,2);
-            dir = zeros(L,1);
-            tDir = zeros(1,2);
-            for m = 1:1:L
-                data = obj.getParticle(ids(m)); %frame,x,y,z
-                pos(m,:) = data(data(:,1)==frameIndex,2:3);
-                v2 = pos(m,:) - data(data(:,1)==(frameIndex-backLength),2:3);
-                tDir = tDir + v2/norm(v2);
-                if isRef
-                    v1 = data(2,2:3) - data(1,2:3);             
-                else
-                    v1 = [1,0];
-                end
-                if or(v2(1),v2(2))
-                    tmp = dot(v1,v2)/(norm(v1)*norm(v2));
-                    if isRef
-                        dir(m) = tmp;
-                    else if v2(2) > 0
-                            dir(m) = acos(tmp);
-                        else
-                            dir(m) = 2*pi - acos(tmp);
-                        end
-                    end
-                else % in case v2 = [0,0]
-                    dir(m) = nan;
-                end
-            end
-            tmp = dot(tDir,[1,0])/norm(tDir);
-            if tDir(2) > 0
-                tDir = acos(tmp);
+            if isDir
+                data = zeros(L,3); %Velocity,Direction;
             else
-                tDir = 2*pi - acos(tmp);
+                data = zeros(L,1); %Velocity
+            end
+            for m = 1:1:L
+                particle_data = obj.getParticle(ids(m)); %frame,x,y,z
+                pos(m,:) = particle_data(particle_data(:,1)==frame,2:3);
+                history_pos = particle_data(particle_data(:,1)==refFrame,2:3);
+                data(m,1) = norm(pos(m,:) - history_pos);
+                if isDir
+                    data(m,2:3) = (pos(m,:) - history_pos)./data(m,1);
+                    if isRef
+                        refVector = ...
+                            particle_data(particle_data(:,1)==refFrame+1,2:3) - history_pos;
+                        refVector = refVector./norm(refVector);
+                        refVector2 = cross([refVector,0],[0,0,-1]);
+                        T = [refVector',refVector2(1:2)']; % space transform matrix
+                        data(m,2:3) = data(m,2:3) * T;
+                    end
+                end
             end
         end
+        
+        function [ids,pos,msdMat] = msdAtFrame(obj,frame,backLength,tau)
+            refFrame = frame - backLength;
+            ids = intersect(obj.getParticleIDAtTime(refFrame),...
+                            obj.getParticleIDAtTime(frame));
+            L = length(ids);
+            pos = zeros(L,2);
+            msdMat = zeros(L,tau+1);
+            for m = 1:1:L
+                particle_data = obj.getParticle(ids(m)); %frame,x,y,z
+                pos(m,:) = particle_data(particle_data(:,1)==frame,2:3);
+                validXY = particle_data(...
+                    and(particle_data(:,1)>=refFrame,particle_data(:,1)<= frame),2:3);
+                msdMat(m,2:end) = msd(validXY,tau)';              
+            end
+        end
+        
+        function [ids,pos,asym] = asymAtFrame(obj,frame,backLength)
+            refFrame = frame - backLength;
+            ids = intersect(obj.getParticleIDAtTime(refFrame),...
+                            obj.getParticleIDAtTime(frame));
+            L = length(ids);
+            pos = zeros(L,2);
+            asym = zeros(L,1);
+            for m = 1:1:L
+                particle_data = obj.getParticle(ids(m)); %frame,x,y,z
+                pos(m,:) = particle_data(particle_data(:,1)==frame,2:3);
+                validXY = particle_data(...
+                    and(particle_data(:,1)>=refFrame,particle_data(:,1)<= frame),2:3);
+                asym(m) = xy2asym(validXY);
+            end
+        end
+        
+%         function [ids,pos,dir,tDir] = getParticleDirAtTime(obj,frame,isRef,backLength)
+%             ids = intersect(obj.getParticleIDAtTime(frame-backLength),...
+%                             obj.getParticleIDAtTime(frame));
+%             L = length(ids);
+%             pos = zeros(L,2);
+%             dir = zeros(L,1);
+%             tDir = zeros(1,2);
+%             for m = 1:1:L
+%                 data = obj.getParticle(ids(m)); %frame,x,y,z
+%                 pos(m,:) = data(data(:,1)==frame,2:3);
+%                 v2 = pos(m,:) - data(data(:,1)==(frame-backLength),2:3);
+%                 tDir = tDir + v2/norm(v2);
+%                 if isRef
+%                     v1 = data(2,2:3) - data(1,2:3);             
+%                 else
+%                     v1 = [1,0];
+%                 end
+%                 if or(v2(1),v2(2))
+%                     tmp = dot(v1,v2)/(norm(v1)*norm(v2));
+%                     if isRef
+%                         dir(m) = tmp;
+%                     else if v2(2) > 0
+%                             dir(m) = acos(tmp);
+%                         else
+%                             dir(m) = 2*pi - acos(tmp);
+%                         end
+%                     end
+%                 else % in case v2 = [0,0]
+%                     dir(m) = nan;
+%                 end
+%             end
+%             tmp = dot(tDir,[1,0])/norm(tDir);
+%             if tDir(2) > 0
+%                 tDir = acos(tmp);
+%             else
+%                 tDir = 2*pi - acos(tmp);
+%             end
+%         end
         
         function frames = getFrames(obj)
             frames = unique(obj.particleData(:,2));
@@ -173,8 +235,8 @@ classdef ParticleData < handle
     end
     
     methods (Access = private)
-        function L = getLength(obj,index)
-            [L,~] = size(obj.getParticle(index));
+        function L = getLength(obj,pID)
+            [L,~] = size(obj.getParticle(pID));
         end
     end
     
