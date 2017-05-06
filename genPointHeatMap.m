80442d51bce97271a2dbe3a4b5a39cf03bb4b041
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2017-05-06 12:31:16 +0800

    new algorithm to fill HM pixel

--- genPointHeatMap.m
+++ genPointHeatMap.m
@@ -1,77 +1,69 @@
-function [X,Y,Z] = genPointHeatMap(haxes,resolution,points,k,r,varargin)
-    Xstep = range(points(:,1))/(resolution-1);
-    Ystep = range(points(:,2))/(resolution-1);
-    [X,Y] = meshgrid(min(points(:,1)):Xstep:max(points(:,1)),...
-                     min(points(:,2)):Ystep:max(points(:,2)));
+function [X,Y,Z] = genPointHeatMap2(haxes,resolution,points,k,r,power,varargin)
+    Xstep = (range(points(:,1))+2*r)/(resolution-1);
+    Ystep = (range(points(:,2))+2*r)/(resolution-1);
+    [X,Y] = meshgrid((min(points(:,1))-r):Xstep:(max(points(:,1))+r),...
+                     (min(points(:,2))-r):Ystep:(max(points(:,2)))+r);
     Z = zeros(resolution,resolution);
     for x_index = 1:1:resolution
-        Z(:,x_index) = pos2value2([X(:,x_index),Y(:,x_index)],points,k,r);
+        Z(:,x_index) = pos2value2([X(:,x_index),Y(:,x_index)],points,k,r,power);
     end
     if isempty(varargin)
         h = pcolor(haxes,X,Y,Z);
+        set(h,'EdgeColor','none');
+        %h = surf(haxes,X,Y,Z);
     else
         h = pcolor(haxes,X,Y,imfilter(Z,fspecial(varargin{1})));
+        set(h,'EdgeColor','none');
+        %h = surf(haxes,X,Y,imfilter(Z,fspecial(varargin{1})));
     end
-    set(h,'EdgeColor','none');
+%     set(h,'EdgeColor','none');
 %     hold on;
-%     scatter(points(:,1),points(:,2),points(:,3),10,points(:,3),'filled');
+%     scatter3(points(:,1),points(:,2),points(:,3),10,points(:,3),'filled','MarkerEdgeColor','k');
 %     hold off;
 end
 
-% function [ values ] = pos2value(pos,pointsData,r,NNRange,naMethods)
-%     % pos2value(pos,pointsData,r,k,naMethods)
-%     % pointsData : X,Y,Value
-%     L = size(pos,1);
-%     values = zeros(L,1);
-%     [D,I] = pdist2(pointsData(:,1:2),pos,'euclidean','Smallest',NNRange(2));
-%     for m = 1:1:L
-%         NNDistance = D(:,m);
-%         NNIndex = I(:,m);
-%         validNum = sum(NNDistance <= r);
-%         if validNum >= NNRange(1)
-%             validNum = min(NNRange(2),validNum);
-%             validDis = NNDistance(1:validNum);
-%             validIndex = NNIndex(1:validNum);
-%             values(m) = disFunc(validDis,pointsData(validIndex,3));
-%         else
-%             switch(naMethods)
-%                 case 'zeros'
-%                     values(m) = 0;
-%                 case 'min'
-%                     values(m) = min(pointsData(:,3));
-%                 case 'max'
-%                     values(m) = max(pointsData(:,3));
-%                 case 'nan'
-%                     values(m) = nan;
-%             end
-%         end
-%     end 
-% end
 
-function [ values ] = pos2value2(pos,pointsData,K,r)
+function [ values ] = pos2value2(pos,pointsData,K,r,power)
     % pos2value(pos,pointsData,r,k,naMethods)
     % pointsData : X,Y,Value
     L = size(pos,1);
     values = zeros(L,1);
     [D,I] = pdist2(pointsData(:,1:2),pos,'euclidean','Smallest',K);
-    for m = 1:1:L
+    for m = 1:1:L      
         NNDistance = D(1:K,m);
         NNIndex = I(1:K,m);
-        v = disFunc2(NNDistance,pointsData(NNIndex,3)-min(pointsData(:,3)),r);
-        if v < 0.01 * range(pointsData(:,3))
+        %values(m) = disFun(NNDistance,pointsData(NNIndex,3),r,0.5);
+        v = guassianFun(NNDistance,pointsData(NNIndex,3),r,power);
+        %v = disFunc2(NNDistance,pointsData(NNIndex,3)-min(pointsData(:,3)),r);
+        if v < 0.01*min(pointsData(:,3))
             values(m) = nan;
         else
-            values(m) = min(pointsData(:,3)) + v;
+            values(m) = v;
         end
+
         
     end 
 end
 
-% function v = disFunc(distance,values)
-%     weights = exp(-distance);
-%     weights = weights./sum(weights);
-%     v = sum(weights.*values);
-% end
+function v = disFun(d,v0,r,param)
+    tmp = v0 .* (max(0,1-(d./r).^param));
+    v = sum(tmp);
+end
+
+function v = guassianFun(d,v0,r,param)
+    %tmp = (v0./(sigma*sqrt(2*pi))).*exp(-d.^2./(2*sigma^2));
+    b = r^param./(log(100*v0));
+    tmp = v0.*exp(-d.^param./b);
+    v = max(tmp);
+    return;
+%     w = exp(-d.^(param*2));
+%     w = w./sum(w);
+%     v = tmp' * w;
+%     if v>max(v0)
+%         disp(v);
+%     end
+end
+
 function v = disFunc2(distance,values,r)
     weights = max(1 - (1/r).*distance,0);
     if sum(weights) > 1
@@ -79,4 +71,5 @@ function v = disFunc2(distance,values,r)
     end
     v = sum(weights.*values);
 end
+
 
